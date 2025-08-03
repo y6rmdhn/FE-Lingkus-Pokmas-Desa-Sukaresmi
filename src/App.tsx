d2f4492bf640be24ab54a63a6571c692311b5288
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-08-03 11:16:55 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>tesssss</h1>
+      <Button>tessss</Button>
+    </>
+  );
+}
+
+export default App;
